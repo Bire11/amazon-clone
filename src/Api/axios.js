82a945c30364be7ff0de38c6175bcca981247966
unknown <birhanuwoeldmariam@gmail.com>
2024-02-16 23:36:25 +0300

    y

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
  const axiosInstance = axios.create({
-    baseURL:"http://127.0.0.1:5001/clone-666ba/us-central1/api"
+   //  baseURL:"http://127.0.0.1:5001/clone-666ba/us-central1/api"
+    baseURL:"https://amazon-clone-back-u1rp.onrender.com",
  });
 
 
